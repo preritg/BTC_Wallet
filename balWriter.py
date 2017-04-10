426703401e593abffe9c234f3f7a40a423c8524b
preritg <prerit.nith@gmail.com>
2017-04-10 09:15:18 +0530

    final data fetch code

--- balWriter.py
+++ balWriter.py
@@ -4,7 +4,7 @@ import time
 
 def balanceWriter(source, balance_str):
     filename = '/home/incogrev/BTC_Wallet/datafiles/' + source.lower() + 'Balance.csv'
-    f = open(filename, 'a')
+    f = open(filename, 'w')
     try:
         f.write(balance_str)
         f.write('\n')
