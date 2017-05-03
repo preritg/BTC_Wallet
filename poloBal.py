3770575ee9d9d533923bf63eac537f0f17cde6ef
preritg <prerit.nith@gmail.com>
2017-05-03 22:29:40 +0530

    Update poloBal.py

--- poloBal.py
+++ poloBal.py
@@ -27,8 +27,8 @@ def poloniexBalance(poloKey, poloSecret):
     coinBalance = [str(ele) for ele in coinBalance]
     return ','.join(coinBalance)
 
-poloKey = '<key_here>'
-poloSecret = '<secret_key_here>'
+poloKey = 'GS3YLS7O-ZJF4EDR4-XICRGQ4H-HH8TSDKZ'
+poloSecret = '95b1c3ff7467c757cba4a513ea5b7b9981261b0c3910ed6ae81cf020acd9637553257fe9ff726ff6d6e32311ef98f8da3482d1a32e817240b164fe8188752217'
 
 
 balWriter.balanceWriter('poloniex', poloniexBalance(poloKey, poloSecret))
